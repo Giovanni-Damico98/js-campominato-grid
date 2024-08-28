366689b2dba169d409363672e1bff97a55280439
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-08-28 23:01:13 +0200

    js progress

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 // Recupero gli elementi
 const grid = document.getElementById("grid");
 const playButton = document.getElementById("play-button");
+const form = document.querySelector("form");
+const levelSelect = document.getElementById("select-level");
 
 // Funzioni
 // Funzione per la creazione di una cella
@@ -15,13 +17,36 @@ const createCell = (cellNumber) => {
 const onCellClick = (event) => {
   console.log(event.target.innerText);
   //   Aggiungo classe per colorare la cella
-  cell.classList.add("clicked");
+  event.target.classList.add("clicked");
 };
-const startGame = () => {
+
+const startGame = (event) => {
+  // Preparazione
+  // Blocco l'invio del form
+  event.preventDefault();
   // Svuoto la griglia
   grid.innerHTML = "";
   // Cambio il testo del bottone gioca
   playButton.innerText = "Ricomincia";
+  //Prendo il valore dalla tendina
+  const level = levelSelect.value;
+  let rows;
+  let cols;
+  // Decido quante colonne e righe generare
+  switch (level) {
+    case "hard":
+      rows = 7;
+      cols = 7;
+      break;
+    case "normal":
+      rows = 9;
+      cols = 9;
+      break;
+    case "easy":
+      rows = 10;
+      cols = 10;
+  }
+
   //   Genero 100 celle
   for (let i = 1; i <= 100; i++) {
     // Creo una cella
@@ -34,4 +59,4 @@ const startGame = () => {
 };
 
 // Elaborazione
-playButton.addEventListener("click", startGame);
+form.addEventListener("submit", startGame);
